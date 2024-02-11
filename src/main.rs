43a00aa64a243dab3dcff444f3e04b7648eb1103
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2024-02-11 18:29:51 +0400

    ilk proyektim rust dilinde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,160 @@
+use std::io;
+
+// Project 1: Interactive bill manager
+//
+// Summary:
+//   Create a command line bills/expenses manager that runs
+//   interactively. This mini project brings together many of
+//   the concepts learn thus far into a single application.
+//
+//   The user stories/requirements are split into stages.
+//   Fully implement each stage as a complete working program
+//   before making changes for the next stage. Leverage the
+//   compiler by using `cargo check --bin p1` when changing
+//   between stages to help identify adjustments that need
+//   to be made.
+//
+// User stories:
+// * Stage 1:
+//   - I want to add bills, including the name and amount owed.
+//   - I want to view existing bills.
+// * Stage 2:
+//   - I want to remove bills.
+// * Stage 3:
+//   - I want to edit existing bills.
+//   - I want to go back if I change my mind.
+//
+// Tips:
+// * Use the loop keyword to create an interactive menu.
+// * Each menu choice should be it's own function, so you can work on the
+//   the functionality for that menu in isolation.
+// * A vector is the easiest way to store the bills at stage 1, but a
+//   hashmap will be easier to work with at stages 2 and 3.
+
+
+#[derive(Debug, Clone)]
+pub struct Bill {
+    name:String,
+    amount:f64,
+}
+
+pub struct Bills{
+    inner: Vec<Bill>
+}
+
+impl Bills {
+    fn new() -> Self{
+        Self{
+            inner:vec![]
+        }
+    }
+
+    fn add(&mut self, bill: Bill) {
+        self.inner.push(bill)
+    }
+
+    fn get_all(&self) -> Vec<&Bill> {
+        self.inner.iter().collect()
+    }
+}
+
+fn get_input() -> Option<String> {
+    let mut buffer = String::new();
+    while io::stdin().read_line(&mut buffer).is_err() {
+        println!("Please enter your data again")
+    }
+    let input = buffer.trim().to_owned();
+    if &input == "" {
+        None
+    } else {
+        Some(input)
+    }
+}
+
+fn get_bill_amount() -> Option<f64> {
+    println!("Amount: ");
+    loop {
+        let input = match get_input() {
+            Some(input) => input,
+            None=> return None
+        };
+        if &input == "" {
+            return None;
+        }
+
+        let parsed_input: Result<f64, _> = input.parse();
+        match parsed_input {
+            Ok(amount)=>return Some(amount),
+            Err(_)=>println!("Please enter a number"),
+        }
+    }
+}
+
+
+
+
+mod menu {
+    use crate::{get_bill_amount, get_input, Bill, Bills};
+
+    pub fn add_bill(bills: &mut Bills){
+        println!("Bill name:");
+        let name = match get_input() {
+            Some(input)=>input,
+            None=>return,
+        };
+
+        let amount = match get_bill_amount(){
+            Some(amount)=>amount,
+            None=>return,
+
+        };
+
+        let bill = Bill{amount, name};
+        bills.add(bill);
+        println!("Billed added")
+    }
+
+    pub fn view_bills(bills: &Bills) {
+        for bill in bills.get_all() {
+            println!("{:?}", bill)
+        }
+    }
+}
+
+enum MainMenu {
+    AddBill,
+    ViewBill,
+}
+
+impl MainMenu {
+    fn from_str(input: &str) -> Option<MainMenu> {
+        match input {
+            "1" => Some(Self::AddBill),
+            "2" => Some(Self::ViewBill),
+            _ => None,
+        }
+    }
+
+    fn show() {
+        println!("");
+        println!(" == Bill Manager ==");
+        println!("1. Add Bill");
+        println!("2. View Bills");
+        println!("");
+        println!("Enter selection: ");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut bills = Bills::new();
+
+    loop {
+        MainMenu::show();
+        let input = get_input().expect("msno data enteredg");
+        match MainMenu::from_str(input.as_str()) {
+            Some(MainMenu::AddBill) => menu::add_bill(&mut bills),
+            Some(MainMenu::ViewBill) => menu::view_bills(&bills),
+            None => return,
+        }
+    }
 }
